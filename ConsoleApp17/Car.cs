b6c13e1d7e9d61bababfaa4a058157d490dac4ae
KenanNagiyev <788lnmp@code.edu.az>
2022-03-13 21:48:46 +0400

    new file

---
+++ ConsoleApp17/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp17
+{
+    class Car : Vehicle
+    {
+        public string Model;
+        public string Brand;
+        public double ShowInfo;
+        public double FuelCapacity;
+        public double FuelFor1km;
+        public double CurrentFuel;
+        public double Drive;
+
+
+        public Car(string model, string brand, double currentfuel, double drive, double fuelfor1km, double fuelcapacity, int year) 
+
+        {
+            if (currentfuel / drive >= fuelfor1km)
+            {
+                 ShowInfo = currentfuel - drive * fuelfor1km;
+                Console.WriteLine(ShowInfo);
+            }
+            else
+            {
+                Console.WriteLine("ceninizde kifayet qeder benzin yoxdur");
+            }
+            this.Model = model;
+            this.Brand = brand;
+            this.CurrentFuel = currentfuel;
+            this.Drive = drive;
+            this.FuelFor1km = fuelfor1km;
+            this.FuelCapacity = fuelcapacity;
+            this.Year = year;
+            
+        }
+    }
+}
---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car("BMW", "M5", 50, 5, 2.5, 100,2020);
+        }
+    }
+}
